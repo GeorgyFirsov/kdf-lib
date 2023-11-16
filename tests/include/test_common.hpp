cfbb709e30f07c7d49da91373267470b46f1d104
Georgy Firsov <gfirsov007@gmail.com>
2023-11-16 14:00:47 +0300

    chore: test environment

---
+++ tests/include/test_common.hpp
@@ -0,0 +1,27 @@
+/**
+ * @file test_common.hpp
+ * @brief Common header for all test cases.
+ */
+
+#pragma once
+
+
+//
+// Google tests library
+//
+
+#include "gtest/gtest.h"
+
+
+//
+// kdf-lib
+//
+
+#include "kdflib.h"
+
+
+//
+// Helpers
+//
+
+#include "test_data.hpp"
